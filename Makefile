c80ad4acd6e65c4e896a33168ea3fb636ea6d4e9
djuara <d22prw@gmail.com>
2019-01-22 19:41:26 +0100

    Init files

---
+++ Makefile
@@ -0,0 +1,2 @@
+PROGRAM=imu
+include ../../common.mk
---
+++ imu.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "espressif/esp_common.h"
+#include "esp/uart.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "esp8266.h"
+
+const int gpio = 2;
+
+void blinkenTask(void *pvParameters)
+{
+    gpio_enable(gpio, GPIO_OUTPUT);
+    while(1) {
+        gpio_write(gpio, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_write(gpio, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void read_gyro(void *pvParameters)
+{
+    gpio_enable(gpio, GPIO_OUTPUT);
+    while(1) {
+        gpio_write(gpio, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_write(gpio, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void user_init(void)
+{
+    uart_set_baud(0, 115200);
+    xTaskCreate(blinkenTask, "blinkenTask", 256, NULL, 2, NULL);
+    xTaskCreate(blinkenTask, "read_gyro", 256, NULL, 2, NULL);
+}
